c0b7b337398a386e8f3d5906b70ac8d7cb05880c
Lucas Gabriel Martins de Oliveira <104532827+guithom04@users.noreply.github.com>
2024-07-19 21:30:01 -0300

    Update BR_economic_series.r
    Add more series

--- BR_economic_series.r
+++ BR_economic_series.r
@@ -1,5 +1,5 @@
-BR_economic_series <- function(){
-  # YoY
+BR_economic_series1 <- function(){
+  # function perfectly informed market expectations
   yoy = function(series){
     index = 1:length(series)
     for(i in index){
@@ -7,7 +7,6 @@ BR_economic_series <- function(){
     }
     return(ts(na.omit(YoY),start = c(start(series)[1]+1,start(series)[2]),frequency = 12))
   }
-  # function perfectly informed market expectations
   dfexpec <- function(){
     # transformar Q em M, com interpolação.(retirado de NMECSYS - NOWCASTING)
     qtr2month <- function(x, reference_month = 3, interpolation = FALSE){
@@ -245,38 +244,74 @@ BR_economic_series <- function(){
     library(sidrar)
     library(zoo)
     library(lubridate)
+    
     #BR industrial production
     PIM = get_sidra(api = "/t/8888/n1/all/v/11602/p/all/c544/129314/d/v11602%201")
     pim = ts(c(NA,diff(PIM$Valor)), start = c(2002,01), frequency = 12)
     df_pim = data.frame(date = as.Date(time(pim)), pim = pim)
+    
     #Capital Goods
     PIM_BK = get_sidra(api = "/t/8887/n1/all/v/11602/p/all/c543/129278/d/v11602%201")
     pim_bk = ts(PIM_BK$Valor, start = c(2002,01),frequency = 12)
     df_pim_bk = data.frame(date = as.Date(time(pim_bk)), pim_bk = pim_bk)
+    
     #Intermediary goods 
     PIM_BI = get_sidra(api = "/t/8887/n1/all/v/11602/p/all/c543/129283/d/v11602%201")
     pim_bi = ts(PIM_BI$Valor, start = c(2002,01), frequency = 12)
     df_pim_bi = data.frame(date = as.Date(time(pim_bi)), pim_bi = pim_bi)
+    
     #BR Retail Sales
     PMCA = get_sidra(api = "/t/8881/n1/all/v/11709/p/all/c11046/56736/d/v11709%201")
     pmca = ts(c(NA,diff(PMCA$Valor)), start = c(2003,01), frequency = 12)
     df_pmca = data.frame(date = as.Date(time(pmca)), pmca = pmca)
+    
     #Área colhida agricultura
     AREA_COLHIDA <- sidrar::get_sidra(api = "/t/6588/n1/all/v/216/p/all/c48/0")
     area_colhida <- ts(yoy(c(NA,diff(AREA_COLHIDA$Valor))), start = c(2007,10), frequency = 12)
     df_area <- data.frame(date = as.Date(time(area_colhida)), area = area_colhida)
+    
     # Custo de Habitação
     HOUSING <- sidrar::get_sidra(api = "/t/647/n3/11/v/all/p/all/c314/7087/c41/787/d/v51%202")
     housing <- ts(yoy(c(NA,NA,diff(HOUSING$Valor,2))), start = c(2000,02), frequency = 12)
     df_housing <- data.frame(date = as.Date(time(housing)), housing = housing)
+    
+    # pmc veículos 
+    PMC_VEICULOS <- sidrar::get_sidra(api = "/t/8884/n1/all/v/11709/p/all/c11046/56737/d/v11709%201")
+    pmc_veiculos <- ts(PMC_VEICULOS$Valor, start = c(2000,01), frequency = 12)
+    df_pmcv <- data.frame(date = as.Date(time(pmc_veiculos)), pmcv = pmc_veiculos)
+    
+    # pmc combustiveis e lubrificantes
+    PMC_COMB <- sidrar::get_sidra(api = "/t/8883/n1/all/v/11709/p/all/c11046/56736/c85/90671/d/v11709%201")
+    pmc_comb <- ts(PMC_COMB$Valor, start = c(2000,01), frequency = 12)
+    df_pmc_comb <- data.frame(date = as.Date(time(pmc_comb)), pmc_comb = pmc_comb)
+    
+    # supermercado
+    PMC_SUPER <- sidrar::get_sidra(api = "/t/8883/n1/all/v/11709/p/all/c11046/56736/c85/90672/d/v11709%201")
+    pmc_super <- ts(PMC_SUPER$Valor, start = c(2000,01), frequency = 12)
+    df_pmc_super <- data.frame(date = as.Date(time(pmc_super)), pmc_super = pmc_super)
+    
+    
+    # tecidos
+    TECIDOS <- sidrar::get_sidra(api = "/t/8883/n1/all/v/11709/p/all/c11046/56736/c85/90673/d/v11709%201")
+    tecidos <- ts(TECIDOS$Valor, start = c(2000,01), frequency = 12)
+    df_tecidos <- data.frame(date = as.Date(time(tecidos)), tecidos = tecidos)
+    
+    
+    # material de contruç"ao
+    
+      
     # unified dataframe
     df <- Reduce(function(df1, df2) merge(df1, df2, by = "date", all = TRUE),
-                     list(df_pim, 
-                          df_pim_bk,
-                          df_pim_bi,
-                          df_pmca,
-                          df_area,
-                          df_housing))
+                 list(df_pim, 
+                      df_pim_bk,
+                      df_pim_bi,
+                      df_pmca,
+                      df_area,
+                      df_housing,
+                      df_pmcv,
+                      df_pmc_comb,
+                      df_pmc_super,
+                      df_tecidos))
   }
   sidrar <- sidrarGET()
   # function to get real time database specified series from BCB APIs
@@ -287,7 +322,7 @@ BR_economic_series <- function(){
     # IBC_BR = GetBCBData::gbcbd_get_series(id = 24363, first.date = "01-01-2003")
     ibc.br.yoy = ts(yoy(IBC_BR$IBC_BR), start = c(2004,01), frequency = 12)
     df_ibc <- data.frame(date = as.Date(time(ibc.br.yoy)), ibc_br = diff(c(NA,ibc.br.yoy)))
-      
+    
     # NUCI
     nuci <- rbcb::get_series(c(nuci = 24352), start_date = "01-01-2003")
     df_nuci <- data.frame(date = nuci$date[13:length(nuci$date)], nuci = diff(c(NA,yoy(nuci$nuci))))
@@ -353,6 +388,49 @@ BR_economic_series <- function(){
     yoy.credito = ts(yoy(credito$value), start = c(1990,01), frequency = 12)
     credito = ts(yoy.credito, start = c(1990,01), frequency = 12)
     df_credito <- data.frame(date = as.Date(time(credito)), credito = c(credito))
+    ETTJ_12M <- ipeadatar::ipeadata(code = "ANBIMA12_TJTLN1212")
+    ettj_yoy <- yoy(ETTJ_12M$value)
+    ettj_12m <- ts(ettj_yoy, start = c(2001,04), frequency = 12)
+    df_ettj <- data.frame(date = as.Date(time(ettj_1m)), ettj = ettj_1m)
+    CAMBIO_NOMINAL <- ipeadatar::ipeadata(code = "BM12_ERC12")
+    cambio_yoy = yoy(CAMBIO_NOMINAL$value)
+    cambio_nominal <- ts(c(NA, diff(cambio_yoy)), start = c(1954,01), frequency = 12)
+    df_cambio <- data.frame(date = as.Date(time(cambio_nominal)), cambio = cambio_nominal)
+    CDI_OVER <- ipeadatar::ipeadata(code = "BM12_TJCDI12")
+    cdi_over <- ts(c(NA,diff(yoy(CDI_OVER$value))), start = c(1987,03), frequency = 12)
+    df_cdi <- data.frame(date = as.Date(time(cdi_over)), cdi = cdi_over)
+    ICC_FECOM = ipeadatar::ipeadata(code = "FCESP12_IIC12")
+    icc_fecom_yoy <- yoy(ICC_FECOM$value)
+    icc_fecom <- ts(icc_fecom_yoy,start = c(2000,12), frequency = 12)
+    df_icc = data.frame(date = as.Date(time(icc_fecom)), icc = icc_fecom)
+    M2 = ipeadatar::ipeadata(code = "BM12_TIPRINCNY12")
+    m2 = ts(yoy(M2$value), start = c(2000,12), frequency = 12)
+    df_m2 = data.frame(date = as.Date(time(m2)), m2 = m2)
+    IMP = ipeadatar::ipeadata(code = "PAN12_MTV12")
+    imp = ts(c(NA,yoy(IMP$value)), start = c(1996,01), frequency = 12)
+    df_imp = data.frame(date = as.Date(time(imp)), imports = imp)
+    CEE = ipeadatar::ipeadata(code = "ELETRO12_CEECOM12")
+    cee = ts(c(NA,diff(yoy(CEE$value))), start = c(1976,01), frequency = 12)
+    df_cee = data.frame(date = as.Date(time(cee)), Energia = cee)
+    ICEA_FECOM = ipeadatar::ipeadata(code = "FCESP12_IICA12")
+    icea_fecom = ts(yoy(ICEA_FECOM$value), start = c(1995,06), frequency = 12)
+    df_icea = data.frame(date = as.Date(time(icea_fecom)), icea = icea_fecom)
+    IEC_FECOM = ipeadatar::ipeadata(code = "FCESP12_IICF12")
+    iec_fecom = ts(yoy(IEC_FECOM$value), start = c(1995,06), frequency = 12)
+    df_iec = data.frame(date = as.Date(time(iec_fecom)), iec = iec_fecom)
+    EXP = ipeadatar::ipeadata(code = "SECEX12_XBENCAPGCE12")
+    exp = ts(yoy(EXP$value), start = c(1998,01), frequency = 12)
+    df_exp = data.frame(date = as.Date(time(exp)),exports = exp)
+    IOF = ipeadatar::ipeadata(code = "SRF12_IOF12")
+    iof = ts(yoy(IOF$value), start = c(1987,01), frequency = 12)
+    df_iof = data.frame(date = as.Date(time(iof)), iof = iof)
+    NASDAQ = ipeadatar::ipeadata(code = "GM12_NASDAQ12")
+    nasdaq = ts(yoy(NASDAQ$value), start = c(1972,03), frequency = 12)
+    df_nasdaq = data.frame(date = as.Date(nasdaq), nasdaq = nasdaq)
+    DOW_JONES = ipeadatar::ipeadata(code = "GM12_DOW12")
+    dow_jones = ts(yoy(DOW_JONES$value), start = c(1923,01), frequency = 12)
+    df_dowjones = data.frame(date = as.Date(dow_jones), dow_jones = dow_jones)
+    
     # reduce
     df <- Reduce(function(df1, df2) merge(df1, df2, by = "date", all = TRUE),
                  list(df_caged,
@@ -366,7 +444,19 @@ BR_economic_series <- function(){
                       df_idp,
                       df_expinf,
                       df_primario,
-                      df_credito))
+                      df_credito,
+                      df_ettj,
+                      df_cambio,
+                      df_cdi,
+                      df_m2,
+                      df_imp,
+                      df_cee,
+                      df_icea,
+                      df_iec,
+                      df_exp,
+                      df_iof,
+                      df_nasdaq,
+                      df_dowjones))
   }
   ipea <- getipea()
   # now merge the series into one only unbalanced panel.
@@ -375,3 +465,5 @@ BR_economic_series <- function(){
   tail(merged_df)
   return(merged_df)
 }
+df = BR_economic_series1()
+tail(df)
